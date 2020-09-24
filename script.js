a7139f0aab69063f9bbfd0e4630ea89002a5d06f
Shanwaz Ghulam <ghulam.shanwaz@gmail.com>
2020-09-24 21:39:41 +0200

    add progress password

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ btnAfficher.onclick = e => {
     pass_id.type = "password";
   }
 };
+
+function outline(el) {
+  el.onfocus = () => el.parentElement.classList.add("border");
+  el.onblur = () => el.parentElement.classList.remove("border");
+}
+
+outline(mail_id);
+outline(pass_id);
+
+function passwordStrength(pw) {
+  return (
+    /.{8,}/.test(pw) /* at least 8 characters */ *
+    (/.{12,}/.test(pw) /* bonus if longer */ +
+    /[a-z]/.test(pw) /* a lower letter */ +
+    /[A-Z]/.test(pw) /* a upper letter */ +
+    /\d/.test(pw) /* a digit */ +
+      /[^A-Za-z0-9]/.test(pw)) /* a special character */
+  );
+}
+
+let pwInput = document.getElementById("pass_id");
+
+pwInput.addEventListener("keyup", function() {
+  document.getElementById("strength").value = passwordStrength(pwInput.value);
+});
